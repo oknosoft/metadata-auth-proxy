b2fabf1173183b82433cb750fc0fbc9c2c5fa3a6
unpete <info@oknosoft.ru>
2019-08-14 23:00:33 +0500

    заготовка редктора метаданных и схем компоновки

---
+++ src/components/MetaDesigner/MetaDesigner.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import LayoutCSS from 'react-grid-layout/css/styles.css';
+import ResizableCSS from 'react-resizable/css/styles.css';
+import RGL, { WidthProvider } from "react-grid-layout";
+import Draggable from './Draggable';
+import MetaTree from './MetaTree';
+import LoadingMessage from 'metadata-react/DumbLoader/LoadingMessage';
+import meta from './meta';
+import './assets/designer.css';
+
+const ReactGridLayout = WidthProvider(RGL);
+const ltitle = 'Конфигуратор';
+
+
+export default class MetaDesigner extends Component {
+
+  state = {
+    data: null,
+    tree_item: null,
+  };
+
+  componentDidMount() {
+    this.props.drawerClose(() => {
+      this.shouldComponentUpdate(this.props);
+      meta().then((data) => this.setState({data}));
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    const {title} = nextProps;
+    if(title != ltitle) {
+      nextProps.handleIfaceState({
+        component: '',
+        name: 'title',
+        value: ltitle,
+      });
+      return false;
+    }
+    return true;
+  }
+
+  treeSelect = (tree_item) => {
+    this.setState({tree_item});
+  }
+
+  render() {
+    const {data, tree_item} = this.state;
+    return data ?
+      <ReactGridLayout
+        className="layout"
+        draggableHandle=".designer-draggable-handle"
+        cols={12}
+        rowHeight={80}
+      >
+        <div key="a" data-grid={{x: 0, y: 0, w: 3, h: 7, minW: 2, maxW: 4}}>
+          <MetaTree
+            data={data}
+            treeSelect={this.treeSelect}
+          />
+        </div>
+        <div key="b" data-grid={{x: 3, y: 0, w: 6, h: 7, minW: 3, maxW: 8}}>
+          <Draggable title="Код">Код</Draggable>
+        </div>
+        <div key="c" data-grid={{x: 9, y: 0, w: 3, h: 7, minW: 2, maxW: 4}}>
+          <Draggable title="Свойства">{`Свойства ${tree_item && tree_item.name}`}</Draggable>
+        </div>
+      </ReactGridLayout>
+      :
+      <LoadingMessage text="Подготовка данных..."/>;
+  }
+
+}
---
+++ src/components/MetaDesigner/MetaTree.js
@@ -0,0 +1,121 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import {Treebeard, decorators} from 'react-treebeard';
+import treebeardTheme from './assets/treebeard-theme'
+
+import * as filters from './filter';
+
+import Draggable from './Draggable';
+
+import styles from './assets/treebeard-styles';
+
+// Example: Customising The Header Decorator To Include Icons
+decorators.Header = (props) => {
+  const style = props.style;
+  return (
+    <div style={style.base}>
+      <div style={style.title}>
+        <div style={styles.treeview_icon} className={props.node.icon}/>
+        {props.node.name}
+      </div>
+    </div>
+  );
+};
+
+decorators.Toggle = (props) => {
+  const style = props.style;
+  const height = style.height;
+  const width = style.width;
+  let r = (height - 4) / 2;
+  let path = `M${r / 2},${r * 1.5}a${r},${r} 0 1,0 ${r * 2},0a${r},${r} 0 1,0 -${r * 2},0`;
+  if (props.node.toggled === true)
+    path += `M${r * 1.5},${r}l0,${r}`;
+  else
+    path += `M${r},${r * 1.5}l${r},0M${r * 1.5},${r}l0,${r}`;
+
+  return (
+    <div style={style.base}>
+      <div style={style.wrapper}>
+        <svg height={height} width={width}>
+          <path d={path} fill="none" stroke="black"></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+const TreebeardContainer = decorators.Container;
+class Container extends TreebeardContainer {
+  renderToggleDecorator() {
+    const {style, decorators, node} = this.props;
+    return (<decorators.Toggle style={style.toggle} node={node}/>);
+  }
+}
+decorators.Container = Container;
+
+
+export default class MetaTree extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {data: props.data, current: null};
+  }
+
+  onToggle = (node, toggled) => {
+    const {current} = this.state;
+    if (current) {
+      current.active = false;
+    }
+    node.active = true;
+    if (node.children) {
+      node.toggled = toggled;
+    }
+    this.setState({current: node});
+    this.props.treeSelect(node);
+  };
+
+  applyFilter = (v) => {
+    if(v !== undefined) {
+      if(this._timout) clearTimeout(this._timout);
+      this._filter = v;
+      this._timout = setTimeout(this.applyFilter, 600);
+    }
+    else {
+      const {_filter, props: {data}} = this;
+      if (!_filter) {
+        return this.setState({data});
+      }
+      Promise.resolve()
+        .then(() => filters.filterTree(data, _filter))
+        .then((filtered) => filters.expandFilteredNodes(filtered, _filter))
+        .then((filtered) => this.setState({data: filtered}));
+    }
+  };
+
+  onFilterMouseUp = ({target}) => {
+    this.applyFilter(target.value.trim());
+  };
+
+  render() {
+    return (
+      <Draggable title="Метаданные">
+        <div className="designer-search-box">
+          <input type="text"
+                 className="designer-input"
+                 placeholder="Поиск в метаданных..."
+                 onKeyUp={this.onFilterMouseUp}
+          />
+        </div>
+        <div className="designer-tree">
+          <Treebeard
+            data={this.state.data}
+            style={treebeardTheme}
+            onToggle={this.onToggle}
+            decorators={decorators}
+          />
+        </div>
+      </Draggable>
+    );
+  }
+}
