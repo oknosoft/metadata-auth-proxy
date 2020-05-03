20204a68e9c95b36743164354f57bc830acc46a0
unpete <info@oknosoft.ru>
2020-05-04 02:52:09 +0500

    статистика сканирований

--- server/http/paperless/scan.js
+++ server/http/paperless/scan.js
@@ -6,92 +6,132 @@
  * Created by Evgeniy Malyarov on 09.02.2020.
  */
 
-const {end404} = require('../end');
+const {end404, end500} = require('../end');
 const getBody = require('../raw-body');
 
-module.exports = function bar($p, log) {
+module.exports = function scan($p, log) {
 
   const {job_prm: {user_node: auth, server}, adapters: {pouch}, classes: {PouchDB}, utils} = $p;
   if(!pouch.remote.events) {
     pouch.remote.events = new PouchDB(server.eve_url, {skip_setup: true, owner: pouch, adapter: 'http', auth});
   }
 
-  return async function bar(req, res) {
+  function history({query, res}) {
+    return pouch.remote.events.query('history', {startkey: [query.bar, ''], endkey: [query.bar, '\ufff0']})
+      .then((rsp) => {
+        res.end(JSON.stringify(rsp));
+      })
+      .catch((err) => end500({res, err, log}));
+  }
+
+  function stringifyKey(key) {
+    return `${key[0]}${key[1].pad()}${key[2].pad()}${key[3]}${key[4]}`;
+  }
+
+  function events({query, res, method, path}) {
+    if(!query.moment) {
+      query.moment = utils.moment().format('YYYYMMDDHHmmssSSS');
+    }
+
+    if(!query.period || !['month','year'].includes(query.period)) {
+      query.period = 'day';
+    }
+
+    const opts = {};
+    if(query.period === 'day') {
+      const year = parseFloat(query.moment.substr(0, 4));
+      const month = parseFloat(query.moment.substr(4, 2));
+      const day = parseFloat(query.moment.substr(6, 2));
+      if(query.place) {
+        opts.key = [year, month, day, query.user, query.place];
+      }
+      else {
+        opts.startkey = [year, month, day, query.user, ''];
+        opts.endkey = [year, month, day, query.user, '\ufff0'];
+      }
+    }
+    else if(query.period === 'month') {
+      opts.keys = [];
+      const start = utils.moment(`${query.moment.substr(0,4)}-${query.moment.substr(4,2)}-${query.moment.substr(6,2)}`);
+      for(let i = 0; i < 31; i++) {
+        const key = start.subtract(i ? 1 : 0, 'day').format('YYYY-MM-DD').split('-').map(v => parseFloat(v));
+        key.push(query.user, query.place);
+        opts.keys.unshift(key);
+      }
+    }
+    // [ 2020, 2, 9, "ef5294e3-bdf3-11e6-81b5-00155d001639", "furn1" ]
+    // query.totals_only
+    return pouch.remote.events.query('events', opts)
+      .then(({rows}) => {
+        const grouping = new Map();
+        for(const row of rows) {
+          const key = stringifyKey(row.key);
+          if(!grouping.has(key)) {
+            grouping.set(key, {l: 0, d: new Set()});
+          }
+          const v = grouping.get(key);
+          v.l++;
+          v.d.add(row.value);
+        }
+        if(query.totals_only && query.period === 'day') {
+          const v = Array.from(grouping.values())[0];
+          if(v) {
+            res.end(JSON.stringify({l: v.l, d: v.d.size}));
+          }
+          else {
+            res.end(JSON.stringify({l: 0, d: 0}));
+          }
+        }
+        else if(query.totals_only && query.period === 'month') {
+          for(const ckey of opts.keys) {
+            const key = stringifyKey(ckey);
+            const v = grouping.get(key);
+            if(v) {
+              ckey.push(v.l, v.d.size);
+            }
+            else {
+              ckey.push(0, 0);
+            }
+          }
+          res.end(JSON.stringify(opts.keys));
+        }
+        else {
+          end404(res, `${method} ${path}`);
+        }
+      });
+  }
+
+  function raw({query, res}) {
+    const startkey = query.moment ? query.moment.substr(0, 8) : utils.moment().format('YYYYMMDD');
+    return pouch.remote.events.allDocs('history', {
+      startkey,
+      endkey: `${startkey}\ufff0`,
+      include_docs: true,
+    })
+      .then((rsp) => {
+        res.end(JSON.stringify(rsp));
+      })
+      .catch((err) => end500({res, err, log}));
+  }
+
+  return async function scan(req, res) {
 
     const {parsed: {path}, method, query} = req;
     if(method === 'GET') {
-      if(!query.period || !['month','year'].includes(query.period)) {
-        query.period = 'day';
-      }
       if(query.bar) {
-        return Promise.resolve({ok: true})
-          .then((rsp) => {
-            res.end(JSON.stringify(rsp));
-          });
+        return history({query, res});
       }
-      else if(query.user) {
-        if(!query.moment) {
-          query.moment = utils.moment().format('YYYYMMDDHHmmssSSS');
-        }
-
-        const opts = {reduce: true, group: true};
-        if(query.period === 'day') {
-          const year = parseFloat(query.moment.substr(0,4));
-          const month = parseFloat(query.moment.substr(4,2));
-          const day = parseFloat(query.moment.substr(6,2));
-          if(query.place) {
-            opts.key = [year, month, day, query.user, query.place];
-          }
-          else {
-            opts.startkey = [year, month, day, query.user, ''];
-            opts.endkey = [year, month, day, query.user, '\ufff0'];
-          }
-          opts.group_level = 3;
-        }
-        else if(query.period === 'month') {
-          opts.keys = [];
-          const start = utils.moment(`${query.moment.substr(0,4)}-${query.moment.substr(4,2)}-${query.moment.substr(6,2)}`);
-          for(let i = 0; i < 31; i++) {
-            const key = start.subtract(i ? 1 : 0, 'day').format('YYYY-MM-DD').split('-').map(v => parseFloat(v));
-            key.push(query.user, query.place);
-            opts.keys.unshift(key);
-          }
-        }
-        // [ 2020, 2, 9, "ef5294e3-bdf3-11e6-81b5-00155d001639", "furn1" ]
-        // query.totals_only
-        return pouch.remote.events.query('events', opts)
-          .then(({rows}) => {
-            if(query.totals_only && query.period === 'day') {
-              const value = rows[0] ? rows[0].value : {length: 0, distinct: []};
-              value.distinct = value.distinct.length;
-              res.end(JSON.stringify(value));
-            }
-            else if(query.totals_only && query.period === 'month') {
-              for(const key of opts.keys) {
-                if(!rows.some((row) => {
-                  for(let i = 0; i < key.length; i++) {
-                    if(key[i] !== row.key[i]) {
-                      return;
-                    }
-                  }
-                  const {length, distinct} = row.value || {};
-                  key.push(length || 0, distinct ? distinct.length : 0);
-                  return true;
-                })) {
-                  key.push(0, 0);
-                }
-              }
-              res.end(JSON.stringify(opts.keys));
-            }
-            else {
-              end404(res, `${method} ${path}`);
-            }
-          });
+      else if(query.raw) {
+        return raw({query, res});
+      }
+      if(query.user) {
+        return events({query, res, method, path});
       }
       else {
         end404(res, `${method} ${path}`);
       }
     }
+
     else if(method === 'PUT' || method === 'POST') {
       return getBody(req)
         .then((body) => {
@@ -107,7 +147,8 @@ module.exports = function bar($p, log) {
             .catch(() => pouch.remote.doc.get(`_local/${barcode}`))
             .then((rsp) => {
               res.end(JSON.stringify(rsp));
-            });
+            })
+            .catch(() => end404(res, `${method} ${barcode}`));
       });
     }
     else {
