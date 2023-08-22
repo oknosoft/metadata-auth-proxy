551badf9725734c59d0a31a4d356fa0f948fad35
unpete <info@oknosoft.ru>
2023-08-22 09:28:31 +0500

    уточнение в загрузке цен

--- server/http/adm.js
+++ server/http/adm.js
@@ -14,13 +14,13 @@ module.exports = function ($p, log, auth) {
   // подключаем плагины
   require('wb-reports')($p, log, route);
   require('wb-parametric')($p, log, route);
-  require('wb-paperless/server')($p, log, route);
+  require('wb-paperless')($p, log, route);
   require('./planning')($p, log, route);
   require('./restart')($p, log, route);
   //require('./supplier')($p, log, route);
   //require('./foroom')($p, log, route);
   require('./calc_stat')($p, log, route);
-  require('wb-planning/server')($p, log, route);
+  require('wb-planning')($p, log, route);
 
   // формирует json описания продукции заказа
   async function ram_data(req, res) {
