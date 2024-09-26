7ce338ce2924496ca427584abe4efedac6a73c8d
unpete <info@oknosoft.ru>
2024-09-26 22:06:42 +0300

    log_processing

--- server/metadata/log_processing.js
+++ server/metadata/log_processing.js
@@ -1,8 +1,48 @@
 
-module.exports = function log_processing({md, adapters: {pouch}, utils}, log) {
+module.exports = function log_processing({md, adapters: {pouch}, classes}, log) {
+
+  if(!pouch.remote.log) {
+    const {__opts} = pouch.remote.ram;
+    pouch.remote.log = new classes.PouchDB(__opts.name.replace(/ram$/, 'log'), {
+      skip_setup: true,
+      adapter: 'http',
+      owner: pouch,
+      fetch: pouch.fetch,
+    });
+  }
 
   log.processing = function ({doc, action, register, error}) {
-    const key = md.get(doc.class_name);
-    return Promise.resolve();
+    const {id} = doc._metadata();
+    if(!id) {
+      return Promise.resolve();
+    }
+    const _id = `${id}|${doc.ref}|p`;
+    return pouch.remote.log.get(_id)
+      .catch((err) => {
+        if(err.status !== 404) throw err;
+        return {_id, events: []};
+      })
+      .then((logDoc) => {
+        const {_rev} = doc;
+        const actionKey = Object.keys(action)[0];
+        let row = logDoc.events.find(v => v._rev === _rev && v.hasOwnProperty(actionKey) && v.register === register);
+        const moment = new Date().toJSON();
+        if(row) {
+          row.attempt++;
+          row.moment = moment;
+          Object.assign(row, action);
+        }
+        else {
+          row = {_rev, ...action, register, attempt: 1, moment};
+          logDoc.events.push(row);
+        }
+        if(error) {
+          row.error = error.stack || error.message || error;
+        }
+        else if(row.error) {
+          delete row.error;
+        }
+        return pouch.remote.log.put(logDoc);
+      });
   }
 }
